fc0cf75c0151ba95047b2ee07586a5a8de675536
valick18 <valick18@gmail.com>
2021-02-04 16:45:39 +0200

    Exit button - working

---
+++ Food/app/src/main/java/Fragments/ExitFragment.java
@@ -0,0 +1,25 @@
+package Fragments;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+import com.example.food.R;
+
+public class ExitFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+      AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+        alertDialog.setTitle("Вихід");
+        alertDialog.setMessage("Ви впевненні, що хочете вийти?");
+        alertDialog.setCancelable(true).setPositiveButton("Так", (dialogConfirm, index)-> System.exit(0));
+        alertDialog.setNegativeButton("Ні", (cancelDialog,ind)-> cancelDialog.cancel()).create().show();
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
